2ad97ab1816a15c7c1c57ee34f3c61258ee2a7e9
HwanakHong <94178576+Nakhong@users.noreply.github.com>
2022-09-28 01:02:05 +0900

    09/28

--- index.js
+++ index.js
@@ -14,3 +14,35 @@ setInterval(() => {
   imgAll.setAttribute("src", `./assets/${chosenImage}`);
   console.log(chosenImage);
 }, 1500);
+
+// 클릭하면 스무스 하게 내려가기
+
+var aTags = document.querySelectorAll("header a");
+for (var i = 0; i < aTags.length; i++) {
+  aTags[i].onclick = function (e) {
+    e.preventDefault();
+    var target = document.querySelector(this.getAttribute("href"));
+
+    window.scrollTo({
+      behavior: "smooth",
+      top: target.offsetTop,
+    });
+  };
+}
+
+// 맨위로 올려주는 버튼 만들기.
+var div = document.querySelector("#container");
+var aTag = document.createElement("a");
+aTag.innerHTML = "위로 가기";
+aTag.setAttribute("class", "a");
+aTag.setAttribute("href", "");
+div.appendChild(aTag);
+// 맨위로 올려주는 이벤트
+aTag.addEventListener("click", (e) => {
+  e.preventDefault();
+  div.scrollTop = 0;
+  window.scrollTo({
+    behavior: "smooth",
+    top: div.offsetTop,
+  });
+});
