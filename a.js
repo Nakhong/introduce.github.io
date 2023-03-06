1a5714118ef46fc4d919ab4b8307d2d3d133c4d2
hwanak hong <ghdghkwo5@naver.com>
2023-03-06 14:47:49 +0900

    asd

--- a.js
+++
@@ -1,9 +0,0 @@
-const arr = ["A", "B", "C", "A", "B"];
-
-const newArr = [];
-arr.forEach((element) => {
-  if (!newArr.includes(element)) {
-    newArr.push(element);
-  }
-});
-console.log(newArr);
--- index.js
+++ index.js
@@ -1,47 +1,38 @@
-// 이미지 랜덤.
-var photo = document.querySelector(".photo");
-const img1 = ["me1.jpg", "me2.jpg", "me3.jpg", "me4.jpg", "me5.jpg"];
-const imgAll = document.createElement("img");
-let chosenImage = img1[Math.floor(Math.random() * img1.length)];
-imgAll.setAttribute("src", `./assets/${chosenImage}`);
-photo.appendChild(imgAll);
+// 태그를 만들어 놓고 집어 넣으면 됨.
+// 내 사진들
+$("#photo").click(function () {
+  $("#main").html("<h1>내사진들</h1>");
 
-// 이미지 랜덤으로 마우스 오버 하면 달라지기.
+  let req = $.ajax({
+    url: "db.json",
+    dataType: "json",
+  });
 
-console.log(imgAll);
-setInterval(() => {
-  chosenImage = img1[Math.floor(Math.random() * img1.length)];
-  imgAll.setAttribute("src", `./assets/${chosenImage}`);
-}, 1500);
-
-// 클릭하면 스무스 하게 내려가기
-
-var aTags = document.querySelectorAll("header a");
-for (var i = 0; i < aTags.length; i++) {
-  aTags[i].onclick = function (e) {
-    e.preventDefault();
-    var target = document.querySelector(this.getAttribute("href"));
-
-    window.scrollTo({
-      behavior: "smooth",
-      top: target.offsetTop,
-    });
-  };
-}
-
-// 맨위로 올려주는 버튼 만들기.
-var div = document.querySelector("#container");
-var aTag = document.createElement("a");
-aTag.innerHTML = "위로 가기";
-aTag.setAttribute("class", "a");
-aTag.setAttribute("href", "");
-div.appendChild(aTag);
-// 맨위로 올려주는 이벤트
-aTag.addEventListener("click", (e) => {
-  e.preventDefault();
-  div.scrollTop = 0;
-  window.scrollTo({
-    behavior: "smooth",
-    top: div.offsetTop,
+  req.done(function (data, status) {
+    for (let i = 0; i < data.image.length; i++) {
+      let image = `<div class="image"><image src="${data.image[i].image}"/> <p>${data.image[i].p}</p></div>`;
+      $(image).attr;
+      $("#main").append(image);
+    }
   });
 });
+
+//자기소개
+
+// $("#profiles").click(function () {
+//   $("#main").html("<h1>자기소개</h1>");
+//   $("#main").append("<div><ul></ul></div>");
+//   $("#main>div>ul").attr("class", "hi");
+
+//   let req = $.ajax({
+//     url: "db.json",
+//     dataType: "json",
+//   });
+
+//   req.done(function (data, status) {
+//     for (let i = 0; i < data.memo.length; i++) {
+//       let li = `<li>${data.memo[i].li}</li>`;
+//       $(".hi").append(li);
+//     }
+//   });
+// });
