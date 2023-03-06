a94299871b61ae981eb70fa8322516d50df830d8
hwanak hong <ghdghkwo5@naver.com>
2023-03-06 14:58:39 +0900

    asd

--- kakaoMap.js
+++ kakaoMap.js
@@ -1,8 +1,8 @@
 function kakaoMap() {
   var mapContainer = document.getElementById("map"), // 지도를 표시할 div
     mapOption = {
-      center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-      level: 10, // 지도의 확대 레벨
+      center: new kakao.maps.LatLng(37.45553, 127.162874), // 지도의 중심좌표
+      level: 3, // 지도의 확대 레벨
     };
 
   var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
