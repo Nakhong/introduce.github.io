f1089807d5dbaaabfce36166492f001bf27a2194
hwanak hong <ghdghkwo5@naver.com>
2023-03-06 17:42:15 +0900

    szc

--- index.js
+++ index.js
@@ -16,3 +16,19 @@ $("#photo").click(function () {
     }
   });
 });
+
+// $("#time").click(function () {
+//   $("#main").html("<h1>차트보기</h1>");
+//   let req = $.ajax({
+//     url: `https://www.gstatic.com/charts/loader.js`,
+//     dataType: "json",
+//   });
+
+//   req.done(function (data, status) {
+//     for (let i = 0; i < data.image.length; i++) {
+//       let image = `<div class="image"><image src="${data.image[i].image}"/> <p>${data.image[i].p}</p></div>`;
+//       $(image).attr;
+//       $("#main").append(image);
+//     }
+//   });
+// });
